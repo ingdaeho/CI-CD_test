8c1c4e86d1a5ca6f9ffb675bcd036ecfcf8870c4
daeho <dlaeogh8572@gmail.com>
2022-01-28 11:06:19 +0900

    test5

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import styles from './index.less';
 export default function IndexPage() {
   return (
     <div>
-      <h1 className={styles.title}>test4</h1>
+      <h1 className={styles.title}>test5</h1>
     </div>
   );
 }
